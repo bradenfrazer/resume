8e72c6e777afc7560404caad33db8dd12423e274
Braden Frazer <bradenfrazer@gmail.com>
2023-10-30 10:32:14 -0500

    view as PDF

---
+++ components/Toast.tsx
@@ -0,0 +1,24 @@
+import React, { FC, useState } from "react";
+
+type ToastProps = {};
+
+const Toast: FC<ToastProps> = ({}) => {
+  const [visible, setVisible] = useState(true);
+  return visible ? (
+    <div className="z-10 pl-3 border border-gray-650 bg-gray-200 rounded fixed top-3 right-3 text-gray-750 inline-flex items-center gap-1 text-sm overflow-hidden">
+      <a href={"/src/resume.pdf"} className="hover:underline">
+        View as PDF
+      </a>
+      <button
+        onClick={() => setVisible(false)}
+        className="text-lg leading-none pt-2 pb-3 px-3 hover:bg-gray-400"
+      >
+        ×
+      </button>
+    </div>
+  ) : (
+    <></>
+  );
+};
+
+export default Toast;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,14 @@ import Head from "next/head";
 
 import Resume from "../components/Resume";
 import resume from "../data/resume";
+import Toast from "../components/Toast";
 
 const Home: NextPage = () => (
   <>
     <Head>
       <title>Braden Frazer | Resume</title>
     </Head>
-
+    <Toast />
     <Resume resume={resume} />
   </>
 );
