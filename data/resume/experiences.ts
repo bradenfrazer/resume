b12ba3b08fd4201039d36885ba6d0835ee490452
Braden Frazer <bradenfrazer@gmail.com>
2024-02-12 09:56:59 -0600

    add BSWH experience

--- data/resume/experiences.ts
+++ data/resume/experiences.ts
@@ -4,6 +4,20 @@ const experiences: Section = {
   title: "EXPERIENCE",
   subsections: [
     {
+      title: "Baylor Scott & White Health (Contract)",
+      description: "Front End Developer (Brand) • January 2024 - Present",
+      body: [
+        {
+          type: "list",
+          items: [
+            "Ensured adherence to brand standards and the internal design system across BSWH public-facing websites.",
+            "Analyzed and improved UI/UX across public websites after the migration from Sharepoint to Sitecore.",
+            "Established best practices for development within the Brand organization, such as Git source control and project management tooling. ",
+          ],
+        },
+      ],
+    },
+    {
       title: "Dropps",
       titleHref: "https://www.bradenfrazer.com/DroppsUXBrief.pdf",
       description: "UX Engineer • April 2022 - August 2023",
@@ -15,11 +29,9 @@ const experiences: Section = {
             "Ensured consistency between Figma and React components using Storybook.",
             "Backed design and 50+ A/B test initiatives with formal UX studies from Baymard and Nielsen Norman Group.",
             "Converted low-fidelity designs into production code for new website features as the lead frontend engineer.",
-            "Facilitated collaboration of engineering and design.",
             "Implemented WCAG (accessibility) best practices including ARIA roles and color contrast.",
             "Led the migration from Material UI to Radix headless components to improve theming, accessibility, and enable server-side rendering.",
             "Pioneered efforts to move website code from Shopify's liquid engine over to React for greater interactivity.",
-            "Rebuilt customer-facing subscription portal from the ground up, with a brand new single-page UI for all functionality, in less than 3 months.",
           ],
         },
       ],
@@ -31,9 +43,7 @@ const experiences: Section = {
         {
           type: "list",
           items: [
-            "Collaborated with designers to conduct user research and build features to fit client needs.",
-            "Developed new features and maintained existing functionality in Vue/Nuxt on an AI-powered SaaS sales platform for distributors.",
-            "Refined best practices and code standards as Proton's second frontend engineer.",
+            "Collaborated with designers to conduct user research and build features to fit client needs on an AI-powered SaaS sales platform for distributors.",
           ],
         },
       ],
